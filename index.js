c29667a56b402e77b8bdfe48e56132a9439d6c7f
mdfaizan973 <faizan.md9735@gmail.com>
2024-03-10 16:24:03 +0530

    error mila th

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 // inde.js
-const app = require("./src/app");
+const app = require("./app");
 
 const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
